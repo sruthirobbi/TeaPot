50f14c6fc0ad712bdbfc2635a4a08bfa40e7caab
SruthiRobbi <sruthidhar.r@gmail.com>
2020-05-02 15:21:39 -0400

    building project file

---
+++ build/precache-manifest.f29891515e6aeaa8b8abccd4f9f0652d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700891af0cce93d18974656b84fc473b",
+    "url": "/TeaPot/index.html"
+  },
+  {
+    "revision": "f10ead4e3db8d5a93838",
+    "url": "/TeaPot/static/css/main.5b41886a.chunk.css"
+  },
+  {
+    "revision": "615a1ff162357c2fcf37",
+    "url": "/TeaPot/static/js/2.84a3a5ab.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/TeaPot/static/js/2.84a3a5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10ead4e3db8d5a93838",
+    "url": "/TeaPot/static/js/main.ecc17cc1.chunk.js"
+  },
+  {
+    "revision": "8890ba0a64f7d92cfaa8",
+    "url": "/TeaPot/static/js/runtime-main.0647e646.js"
+  },
+  {
+    "revision": "cb17b476f53897d775d6ca542738f10f",
+    "url": "/TeaPot/static/media/HibiscusTea.cb17b476.jpg"
+  },
+  {
+    "revision": "1b34f06a3b026efe804465747b7ad6bc",
+    "url": "/TeaPot/static/media/black_tea.1b34f06a.jpg"
+  },
+  {
+    "revision": "6bc33a1f328aced27fd1909a1ec32172",
+    "url": "/TeaPot/static/media/img8.6bc33a1f.jpg"
+  },
+  {
+    "revision": "0e4a3eb466d9d8f1a25387737784ec9f",
+    "url": "/TeaPot/static/media/masala__tea.0e4a3eb4.jpg"
+  },
+  {
+    "revision": "78c075014fb6acf46a90bb323081371b",
+    "url": "/TeaPot/static/media/matcha_tea.78c07501.jpg"
+  },
+  {
+    "revision": "f59fa3e08d5d66ba203631b4b30d76c6",
+    "url": "/TeaPot/static/media/peppermint_tea.f59fa3e0.jpg"
+  },
+  {
+    "revision": "eacca5ede87bb738c68325d6893742b0",
+    "url": "/TeaPot/static/media/tea-amillionstyles.eacca5ed.jpg"
+  },
+  {
+    "revision": "2e9ea7a52609909623afa4bd18f88a7b",
+    "url": "/TeaPot/static/media/termaric_ginger_tea.2e9ea7a5.jpg"
+  },
+  {
+    "revision": "eacca5ede87bb738c68325d6893742b0",
+    "url": "/TeaPot/static/media/typeOfTea1.eacca5ed.jpg"
+  },
+  {
+    "revision": "e9a0d91a5eb480662c0e25c126021c0f",
+    "url": "/TeaPot/static/media/typeOfTea2.e9a0d91a.jpg"
+  },
+  {
+    "revision": "e9a0d91a5eb480662c0e25c126021c0f",
+    "url": "/TeaPot/static/media/typeOfTea3.e9a0d91a.jpg"
+  },
+  {
+    "revision": "a876606235dcb3b058f2e675e4d93683",
+    "url": "/TeaPot/static/media/typeOfTea4.a8766062.jpg"
+  },
+  {
+    "revision": "e9a0d91a5eb480662c0e25c126021c0f",
+    "url": "/TeaPot/static/media/types-of-tea.e9a0d91a.jpg"
+  },
+  {
+    "revision": "2c1c7066b947844d76b3b1b70eeff545",
+    "url": "/TeaPot/static/media/white_tea.2c1c7066.jpg"
+  },
+  {
+    "revision": "48b550b8e6cb38bb97c664bcace78aac",
+    "url": "/TeaPot/static/media/yellow_tea.48b550b8.jpg"
+  }
+]);
