b0b7b63efc6ca819a609ec7e4c386f73ad260c7e
SruthiRobbi <sruthidhar.r@gmail.com>
2020-05-03 15:18:46 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TeaPot/precache-manifest.6d231ede41b74ccce0a45174b5b9b0f1.js"
+  "/TeaPot/precache-manifest.37c4631dfc88ae8ce31a6919c4b193f3.js"
 );
 
 self.addEventListener('message', (event) => {
